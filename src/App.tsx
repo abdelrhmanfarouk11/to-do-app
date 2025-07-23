d1a5aa5249dddd106001076d4cc35f4db770b67d
abdelrhmanfarouk11 <youremail@example.com>
2025-07-23 15:59:51 +0300

    fixing the context issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 // App.tsx
-import { ThemeProvider } from "./context/themeContext";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <ThemeProvider>
-      <Navbar/>
+    <div>
+      <Navbar />
       <Home />
-    </ThemeProvider>
+    </div>
   );
 };
 
